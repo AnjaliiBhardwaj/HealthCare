500f5f971521d65ae4d842a98d14694f4c1c24fa
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2024-06-19 11:48:50 +0530

    Initial commit

---
+++ Backend/middlewares/auth.js
@@ -0,0 +1,57 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/userSchema.js";
+import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
+import ErrorHandler from "../middlewares/errorMiddleware.js";
+
+export const isAdminAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  const token = req.cookies.adminToken;
+  if (!token) {
+    return next(new ErrorHandler("Admin Not Authenticated!", 400));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  req.user = await User.findById(decoded.id);
+
+  if (!req.user) {
+    return next(new ErrorHandler("User not found!", 404));
+  }
+
+  if (req.user.role !== "Admin") {
+    return next(
+      new ErrorHandler(
+        `${req.user.role} not authorized for this resource!`,
+        403
+      )
+    );
+  }
+
+  next();
+});
+
+// Middleware to authenticate frontend users
+export const isPatientAuthenticated = catchAsyncErrors(
+  async (req, res, next) => {
+    const token = req.cookies.patientToken;
+    if (!token) {
+      return next(new ErrorHandler("User is not authenticated!", 400));
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = await User.findById(decoded.id);
+
+    if (!req.user) {
+      return next(new ErrorHandler("User not found!", 404));
+    }
+
+    if (req.user.role !== "Patient") {
+      return next(
+        new ErrorHandler(
+          `${req.user.role} not authorized for this resource!`,
+          403
+        )
+      );
+    }
+
+    next();
+  }
+);
---
+++ Backend/router/userRouter.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  addNewAdmin,
+  addNewDoctor,
+  getAllDoctors,
+  getUserDetails,
+  login,
+  logoutAdmin,
+  logoutPatient,
+  patientRegister,
+} from "../controller/userController.js";
+import {
+  isAdminAuthenticated,
+  isPatientAuthenticated,
+} from "../middlewares/auth.js";
+import { get } from "mongoose";
+const router = express.Router();
+
+router.post("/patient/register", patientRegister);
+router.post("/login", login);
+router.post("/admin/addnew", isAdminAuthenticated, addNewAdmin);
+router.get("/doctors", getAllDoctors);
+router.get("/admin/me", isAdminAuthenticated, getUserDetails); //isAmin.. is a middleware, if it works successfully then the func wil work
+router.get("/patient/me", isPatientAuthenticated, getUserDetails);
+router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
+router.get("/patient/logout", isPatientAuthenticated, logoutPatient);
+router.post("/doctor/addnew", isAdminAuthenticated, addNewDoctor);
+export default router;
